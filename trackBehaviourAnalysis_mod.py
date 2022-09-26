d47322b14f0e8756a50662615344cffd6c9ee719
carlacodes <carla.griffiths@gmail.com>
2022-09-26 23:09:00 +0100

    fixing errors

--- trackBehaviourAnalysis_mod.py
+++ trackBehaviourAnalysis_mod.py
@@ -5,11 +5,12 @@ from instruments.config import behaviouralDataPath, behaviourOutput
 from instruments.behaviouralAnalysis import createWeekBehaviourFigs, reactionTimeAnalysis, outputbehaviordf
 import math
 import numpy as np
-
+from instruments.helpers.extract_helpers import extractAllFerretData
 import pandas as pd
 import statsmodels.api as sm
 import statsmodels.formula.api as smf
 from statsmodels.tools.sm_exceptions import ConvergenceWarning
+
 
 # jules' extract behvioural data analysis
 # @click.group()
@@ -112,44 +113,50 @@ def get_df_behav(path=None,
                                finishDate=finishdate,
                                ferrets=ferrets,
                                outDir=output)
+    # allData, ferrets = extractAllFerretData(ferrets, path, startDate=startdate,
+    # finishDate=finishdate)
 
     allData = dataSet._load()
 
     # for ferret in ferrets:
-
-    #ferrData = allData.loc[allData.ferretname == ferret]
+    ferret = ferrets
+    ferrData = allData.loc[allData.ferretname == ferret]
     # if ferret == 'F1702_Zola':
     #     ferrData = ferrData.loc[(ferrData.dates != '2021-10-04 10:25:00')]
+    allData = allData[allData['catchTrial'].isin([0])]
+    #allData = allData[allData['response'].isin(['0', '1'])]
     pitchshiftmat = allData['PitchShiftMat']
     precursorlist = allData['distractors']
     pitchoftarg = np.empty(len(pitchshiftmat))
-    pitchofprecur=np.empty(len(pitchshiftmat))
+    pitchofprecur = np.empty(len(pitchshiftmat))
+
     for i in range(0, len(pitchshiftmat)):
         chosentrial = pitchshiftmat[i]
         chosendisttrial = precursorlist[i]
 
         targpos = np.where(chosendisttrial == 1)
-
-        if not isinstance(chosentrial, (np.ndarray, np.generic)):
-            if math.isnan(chosentrial):
-                chosentrial = np.zeros(5)
-        try:
-            chosentrial = chosentrial[chosentrial != 0]
-        except:
-            continue
-        if chosentrial.size == 0:
-            pitchoftarg[i] = 0
-
-        else:
-            try:
-                pitchoftarg[i] = chosentrial[targpos]
-                pitchofprecur[i]=chosentrial[targpos-1]
-            except:
-                pitchoftarg[i] = 0
-                pitchofprecur[i] = 0
+        pitchoftarg[i] = chosentrial[targpos[0] - 1]
+        pitchofprecur[i] = chosentrial[targpos[0] - 2]
+        # if not isinstance(chosentrial, (np.ndarray, np.generic)):
+        #     if math.isnan(chosentrial):
+        #         chosentrial = np.zeros(5)
+        # try:
+        #     chosentrial = chosentrial[chosentrial != 0]
+        # except:
+        #     continue
+        # if chosentrial.size == 0:
+        #     pitchoftarg[i] = 0
+        #
+        # else:
+        #     try:
+        #         pitchoftarg[i] = int(chosentrial[targpos])
+        #         pitchofprecur[i] = chosentrial[targpos - 1]
+        #     except:
+        #         pitchoftarg[i] = 0
+        #         pitchofprecur[i] = 0
 
     allData['pitchoftarg'] = pitchoftarg.tolist()
-    allData['pitchofprecur']=pitchofprecur.tolist()
+    allData['pitchofprecur'] = pitchofprecur.tolist()
 
     return allData
 
@@ -161,17 +168,16 @@ def get_df_behav(path=None,
 # cli.add_command(cli_reaction_time)
 
 if __name__ == '__main__':
-    ferrets = "F1702_Zola", "F1815_Cruella"
-    for i, currFerr in enumerate(ferrets):
-        print(i, currFerr)
+    ferrets = 'F1702_Zola'
+    # for i, currFerr in enumerate(ferrets):
+    #     print(i, currFerr)
     df = get_df_behav(ferrets=ferrets, startdate='04-01-2020', finishdate='04-01-2022')
-    #cli_reaction_time(ferrets='F1702_Zola', startdate='04-01-2020', finishdate='04-01-2022')
+    # cli_reaction_time(ferrets='F1702_Zola', startdate='04-01-2020', finishdate='04-01-2022')
     data = sm.datasets.get_rdataset("Sitka", "MASS").data
-    endog = data["size"]
-    endog = df['response']
+    endog = df['timeToTarget']
     data["Intercept"] = 1
     exog = df[["pitchoftarg", "pitchofprecur"]]
-    exog["Intercept"]=1
+    exog["Intercept"] = 1
     md = sm.MixedLM(endog, exog, groups=df["ferret"], exog_re=exog["Intercept"])
     mdf = md.fit()
     print(mdf.summary())
