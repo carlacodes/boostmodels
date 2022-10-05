904f042b33775702938dfc4b3b8ea863de442872
carlacodes <carla.griffiths@gmail.com>
2022-10-05 12:16:42 +0100

    successfully implemented this lasso feature selection !!!!

--- trackBehaviourAnalysis_mod.py
+++ trackBehaviourAnalysis_mod.py
@@ -18,7 +18,7 @@ from pysr3.linear.models import LinearL1ModelSR3
 from sklearn.metrics import accuracy_score, confusion_matrix
 from sklearn.model_selection import RandomizedSearchCV
 from sklearn.utils.fixes import loguniform
-
+import statistics as stats
 
 # jules' extract behvioural data analysis
 # @click.group()
@@ -202,14 +202,20 @@ if __name__ == '__main__':
     # cli_reaction_time(ferrets='F1702_Zola', startdate='04-01-2020', finishdate='04-01-2022')
     data = sm.datasets.get_rdataset("Sitka", "MASS").data
     endog = df['realRelReleaseTimes']
+    endog2 = df['realRelReleaseTimes'].to_numpy()
+
     data["Intercept"] = 1
     exog = df[["pitchoftarg", "pitchofprecur", "talker"]]
+    exog2 = df[["ferret", "pitchoftarg", "pitchofprecur", "talker"]].to_numpy()
+    varianceofarray=np.var(exog2, axis=1)
+    exog2 = np.insert(exog2, 1, varianceofarray, axis=1)
     exog["Intercept"] = 1
     md = sm.MixedLM(endog, exog, groups=df["ferret"], exog_re=exog["Intercept"])
     mdf = md.fit(reml=False)
     print(mdf.summary())
     print(mdf.aic)
-    print(mdf.bic
+    print(mdf.bic)
+    seed = 42
     # print(mdf.params)
     model = L1LmeModelSR3()
     # x is  a long array of dependent VARS
@@ -222,31 +228,33 @@ if __name__ == '__main__':
         "lam": loguniform(1e-3, 1e3)
     }
     # We use standard functionality of sklearn to perform grid-search.
+    column_labels = ['group'] * 1 + ["fixed+random"] * 3+['variance'] * 1
     selector = RandomizedSearchCV(estimator=model,
                                   param_distributions=params,
                                   n_iter=10,  # number of points from parameters space to sample
                                   # the class below implements CV-splits for LME models
                                   cv=LMEStratifiedShuffleSplit(n_splits=2, test_size=0.5,
-                                                               random_state=seed,
-                                                               columns_labels=columns_labels),
+                                                               random_state=seed, columns_labels=column_labels),
                                   # The function below will evaluate the information criterion
                                   # on the test-sets during cross-validation.
                                   # We use IC from Muller2018, but other options (AIC, BIC) are also available
-                                  scoring=lambda clf, x, y: -clf.get_information_criterion(exog,endog, columns_labels =                                                                                           columns_labels=columns_labels,
-                                                                                           ic="muller_ic"),
+                                  scoring=lambda clf, exog2, endog2: -clf.get_information_criterion(exog2, endog2,
+                                                                                                    columns_labels=column_labels,
+                                                                                                    ic="muller_ic"),
                                   random_state=seed,
                                   n_jobs=20
                                   )
-    selector.fit(x, y, columns_labels=columns_labels)
+    selector.fit(exog2, endog2, columns_labels=column_labels)
     best_model = selector.best_estimator_
 
     maybe_beta = best_model.coef_["beta"]
     maybe_gamma = best_model.coef_["gamma"]
-    # returning a binary array of 0 1 true false to get the actually relevant values?? -- yes as in the sk learn function y_true is the first argument and y_pred is the second argument
-    ftn, ffp, ffn, ftp = confusion_matrix(true_parameters["beta"], abs(maybe_beta) > 1e-2).ravel()
-    rtn, rfp, rfn, rtp = confusion_matrix(true_parameters["gamma"], abs(maybe_gamma) > 1e-2).ravel()
-    print(
-        f"The model_name found {ftp} out of {ftp + ffn} correct fixed features, and also chose {ffp} out of {ftn + ffn} extra irrelevant fixed features. \n"
-        f"It also identified {rtp} out of {rtp + rfn} random effects correctly, and got {rfp} out of {rtn + rfn} non-present random effects. \n"
-        f"The best sparsity parameter is {selector.best_params_}")
-
+    beta_coeffs_to_use = abs(maybe_beta) > 1e-2
+    gamma_coeffs_to_use = abs(maybe_gamma) > 1e-2
+    # # returning a binary array of 0 1 true false to get the actually relevant values?? -- yes as in the sk learn function y_true is the first argument and y_pred is the second argument
+    # ftn, ffp, ffn, ftp = confusion_matrix(true_parameters["beta"], abs(maybe_beta) > 1e-2).ravel()
+    # rtn, rfp, rfn, rtp = confusion_matrix(true_parameters["gamma"], abs(maybe_gamma) > 1e-2).ravel()
+    # print(
+    #     f"The model_name found {ftp} out of {ftp + ffn} correct fixed features, and also chose {ffp} out of {ftn + ffn} extra irrelevant fixed features. \n"
+    #     f"It also identified {rtp} out of {rtp + rfn} random effects correctly, and got {rfp} out of {rtn + rfn} non-present random effects. \n"
+    #     f"The best sparsity parameter is {selector.best_params_}")
