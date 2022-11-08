4e3b106d0fd3fade5ee18f279e9d9178add22e87
carlacodes <carla.griffiths@gmail.com>
2022-11-08 18:54:19 +0000

    using kaggle code to run optuna study source: https://towardsdatascience.com/kagglers-guide-to-lightgbm-hyperparameter-tuning-with-optuna-in-2021-ed048d9838b5

--- generatemixedeffectsmodelsmodv3simplified.py
+++ generatemixedeffectsmodelsmodv3simplified.py
@@ -18,6 +18,8 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 import shap
 import lightgbm as lgb
+import optuna
+from optuna.integration import LightGBMPruningCallback
 
 scaler = MinMaxScaler()
 import os
@@ -665,8 +667,7 @@ def runlgbreleasetimes(df_use):
     shap.plots.scatter(shap_values2[:, "pitchoftarg"], color=shap_values2[:, "talker"])
     plt.title('Reaction Time Model')
     plt.show()
-    shap.plots.scatter(shap_values2[:, "trialNum"], color=shap_values2[:, "talker"])
-    plt.title('Reaction Time Model')
+    shap.plots.scatter(shap_values2[:, "trialNum"], color=shap_values2[:, "talker"], title='Correct Responses - Reaction Time Model SHAP response \n vs. trial number')
 
     plt.show()
 
@@ -740,6 +741,61 @@ def runlgbcorrectresponse(dfcat_use):
 
     return xg_reg, ypred, y_test, results, shap_values
 
+
+def objective(trial, X, y):
+    param_grid = {
+        # "device_type": trial.suggest_categorical("device_type", ['gpu']),
+        "n_estimators": trial.suggest_categorical("n_estimators", [10000]),
+        "learning_rate": trial.suggest_float("learning_rate", 0.01, 0.3),
+        "num_leaves": trial.suggest_int("num_leaves", 20, 3000, step=20),
+        "max_depth": trial.suggest_int("max_depth", 3, 12),
+        "min_data_in_leaf": trial.suggest_int("min_data_in_leaf", 200, 10000, step=100),
+        "lambda_l1": trial.suggest_int("lambda_l1", 0, 100, step=5),
+        "lambda_l2": trial.suggest_int("lambda_l2", 0, 100, step=5),
+        "min_gain_to_split": trial.suggest_float("min_gain_to_split", 0, 15),
+        "bagging_fraction": trial.suggest_float(
+            "bagging_fraction", 0.2, 0.95, step=0.1
+        ),
+        "bagging_freq": trial.suggest_categorical("bagging_freq", [1]),
+        "feature_fraction": trial.suggest_float(
+            "feature_fraction", 0.2, 0.95, step=0.1
+        ),
+    }
+
+    cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=1121218)
+
+    cv_scores = np.empty(5)
+    for idx, (train_idx, test_idx) in enumerate(cv.split(X, y)):
+        X_train, X_test = X.iloc[train_idx], X.iloc[test_idx]
+        y_train, y_test = y[train_idx], y[test_idx]
+
+        model = lgbm.LGBMClassifier(objective="binary", **param_grid)
+        model.fit(
+            X_train,
+            y_train,
+            eval_set=[(X_test, y_test)],
+            eval_metric="binary_logloss",
+            early_stopping_rounds=100,
+            callbacks=[
+                LightGBMPruningCallback(trial, "binary_logloss")
+            ],  # Add a pruning callback
+        )
+        preds = model.predict_proba(X_test)
+        cv_scores[idx] = log_loss(y_test, preds)
+
+    return np.mean(cv_scores)
+
+
+def run_optuna_study_correctresp(X,y):
+    study = optuna.create_study(direction="minimize", study_name="LGBM Classifier")
+    func = lambda trial: objective(trial, X, y)
+    study.optimize(func, n_trials=20)
+    print("Number of finished trials: ", len(study.trials))
+    print(f"\tBest value (rmse): {study.best_value:.5f}")
+    print(f"\tBest params:")
+
+    for key, value in study.best_params.items():
+        print(f"\t\t{key}: {value}")
 if __name__ == '__main__':
     ferrets = ['F1702_Zola', 'F1815_Cruella', 'F1803_Tina', 'F2002_Macaroni']
     modelreg_reduc, modelregcat_reduc, modelregcat, modelreg, predictedrelease, df_use, dfcat_use, predictedcorrectresp, explainedvar, explainvarreleasetime = run_mixed_effects_analysis(
