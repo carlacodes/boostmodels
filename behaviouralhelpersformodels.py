b6492e2acd46bb90ea49068e61e2669f4f81f20c
carlacodes <carla.griffiths@gmail.com>
2023-02-16 15:31:51 +0000

    fixing errors in helper functions

--- behaviouralhelpersformodels.py
+++ behaviouralhelpersformodels.py
@@ -476,8 +476,8 @@ class behaviouralhelperscg:
             #distractor_or_fa = np.delete(distractor_or_fa, 0)
             stepval = np.delete(stepval, 0)
             pitchofprecur = np.delete(pitchofprecur, 0)
-            control_trial = np.delete(control_trial, 0)
-            intra_trial_roving = np.delete(intra_trial_roving, 0)
+            #control_trial = np.delete(control_trial, 0)
+            #intra_trial_roving = np.delete(intra_trial_roving, 0)
 
 
             newdata['pitchoftarg'] = pitchoftarg.tolist()
