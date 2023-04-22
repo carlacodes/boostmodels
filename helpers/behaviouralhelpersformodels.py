bef6d9ebfbd5abb099e0a0af370c1307ee61ab38
carlacodes <carla.griffiths@gmail.com>
2023-04-22 13:22:31 +0100

    move balanced_subsample to helpers

--- helpers/behaviouralhelpersformodels.py
+++ helpers/behaviouralhelpersformodels.py
@@ -571,3 +571,34 @@ class behaviouralhelperscg():
             # newdata['distindex'] = emptydistracotrindexdict_categorical
             bigdata = bigdata.append(newdata)
         return bigdata
+
+    def balanced_subsample(x, y, subsample_size=1.0):
+        class_xs = []
+        min_elems = None
+
+        for yi in np.unique(y):
+            elems = x[(y == yi)]
+            class_xs.append((yi, elems))
+            if min_elems == None or elems.shape[0] < min_elems:
+                min_elems = elems.shape[0]
+
+        use_elems = min_elems
+        if subsample_size < 1:
+            use_elems = int(min_elems * subsample_size)
+
+        xs = []
+        ys = []
+
+        for ci, this_xs in class_xs:
+            if len(this_xs) > use_elems:
+                this_xs = this_xs.reindex(np.random.permutation(this_xs.index))
+            x_ = this_xs[:use_elems]
+            y_ = np.empty(use_elems)
+            y_.fill(ci)
+
+            xs.append(x_)
+            ys.append(y_)
+
+        xs = pd.concat(xs)
+        ys = pd.Series(data=np.concatenate(ys), name='target')
+        return xs, ys
--- runlightgbmmodels.py
+++ runlightgbmmodels.py
@@ -541,36 +541,7 @@ def runlgbreleasetimes_for_a_ferret(data, paramsinput=None, ferret=1, ferret_nam
     return xg_reg, ypred, y_test, results, mse
 
 
-def balanced_subsample(x, y, subsample_size=1.0):
-    class_xs = []
-    min_elems = None
 
-    for yi in np.unique(y):
-        elems = x[(y == yi)]
-        class_xs.append((yi, elems))
-        if min_elems == None or elems.shape[0] < min_elems:
-            min_elems = elems.shape[0]
-
-    use_elems = min_elems
-    if subsample_size < 1:
-        use_elems = int(min_elems * subsample_size)
-
-    xs = []
-    ys = []
-
-    for ci, this_xs in class_xs:
-        if len(this_xs) > use_elems:
-            this_xs = this_xs.reindex(np.random.permutation(this_xs.index))
-        x_ = this_xs[:use_elems]
-        y_ = np.empty(use_elems)
-        y_.fill(ci)
-
-        xs.append(x_)
-        ys.append(y_)
-
-    xs = pd.concat(xs)
-    ys = pd.Series(data=np.concatenate(ys), name='target')
-    return xs, ys
 
 
 def runlgbcorrectresponse(dfx, dfy, paramsinput=None):
