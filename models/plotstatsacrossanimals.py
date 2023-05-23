dfa2f2e0bbfef390d4e559692b282eefcd373e6f
carlacodes <carla.griffiths@gmail.com>
2023-05-23 12:54:38 +0100

    changing correct reject to cor4rect responses

--- models/plotstatsacrossanimals.py
+++ models/plotstatsacrossanimals.py
@@ -34,7 +34,7 @@ def run_stats_calc(df, ferrets, stats_dict, pitch_param = 'control_trial'):
     # stats_dict[pitch_param] = {}
     # stats_dict[pitch_param]['hits'] = {}
     # stats_dict[pitch_param]['false_alarms'] = {}
-    # stats_dict[pitch_param]['correct_rejections'] = {}
+    # stats_dict[pitch_param]['correct_response'] = {}
     talkers = [1,2]
     stats_dict[1] = {}
     stats_dict[2] = {}
@@ -43,13 +43,13 @@ def run_stats_calc(df, ferrets, stats_dict, pitch_param = 'control_trial'):
 
     stats_dict[1][pitch_param]['hits'] = {}
     stats_dict[1][pitch_param]['false_alarms']= {}
-    stats_dict[1][pitch_param]['correct_rejections']= {}
+    stats_dict[1][pitch_param]['correct_response']= {}
     stats_dict[1][pitch_param]['dprime']= {}
 
 
     stats_dict[2][pitch_param]['hits'] ={}
     stats_dict[2][pitch_param]['false_alarms'] = {}
-    stats_dict[2][pitch_param]['correct_rejections'] = {}
+    stats_dict[2][pitch_param]['correct_response'] = {}
     stats_dict[2][pitch_param]['dprime']= {}
 
     count = 0
@@ -59,12 +59,17 @@ def run_stats_calc(df, ferrets, stats_dict, pitch_param = 'control_trial'):
         selected_ferret_catch = df_catchnoncorrection[df_catchnoncorrection['ferret'] == count]
         for talker in talkers:
             selected_ferret_talker = selected_ferret[selected_ferret['talker'] == talker]
+
+            selected_ferret_talker_hitrate = selected_ferret_talker[selected_ferret_talker['response'] != 5]
+
             selected_ferret_catch_talker = selected_ferret_catch[selected_ferret_catch['talker'] == talker]
 
-            stats_dict[talker][pitch_param]['hits'][ferret] = np.mean(selected_ferret_talker['hit'])
+            stats_dict[talker][pitch_param]['hits'][ferret] = np.mean(selected_ferret_talker_hitrate['hit'])
             stats_dict[talker][pitch_param]['false_alarms'][ferret] = np.mean(selected_ferret_talker['falsealarm'])
             stats_dict[talker][pitch_param]['dprime'][ferret] = CalculateStats.dprime(np.mean(selected_ferret_talker['hit']), np.mean(selected_ferret_talker['falsealarm']))
-            stats_dict[talker][pitch_param]['correct_rejections'][ferret] = np.mean(selected_ferret_catch_talker['response'] == 3)
+
+            #%Correct(hit + CR / hits + misses + CR + FA)
+            stats_dict[talker][pitch_param]['correct_response'][ferret] = (len(selected_ferret_talker['hit']==True) + len(selected_ferret_catch_talker['response'] == 3))/ (len(selected_ferret_talker) + len(selected_ferret_catch_talker))
         count += 1
     stats_dict_all = {}
     stats_dict_all[1] ={}
@@ -75,14 +80,17 @@ def run_stats_calc(df, ferrets, stats_dict, pitch_param = 'control_trial'):
 
     for talker in talkers:
         df_noncatchnoncorrection_talker = df_noncatchnoncorrection[df_noncatchnoncorrection['talker'] == talker]
+        df_noncatchnoncorrection_talker_hitrate = df_noncatchnoncorrection_talker[df_noncatchnoncorrection_talker['response'] != 5]
+
         df_catchnoncorrection_talker = df_catchnoncorrection[df_catchnoncorrection['talker'] == talker]
-        hits = np.mean(df_noncatchnoncorrection_talker['hit'])
+        hits = np.mean(df_noncatchnoncorrection_talker_hitrate['hit'])
         false_alarms = np.mean(df_noncatchnoncorrection_talker['falsealarm'])
         correct_rejections = np.mean(df_catchnoncorrection_talker['response'] == 3)
+        correct_response =  (len(df_noncatchnoncorrection_talker['hit']==True) + len(df_catchnoncorrection_talker['response'] == 3))/ (len(df_noncatchnoncorrection_talker) + len(df_catchnoncorrection_talker))
 
         stats_dict_all[talker][pitch_param]['hits'] = hits
         stats_dict_all[talker][pitch_param]['false_alarms'] = false_alarms
-        stats_dict_all[talker][pitch_param]['correct_rejections'] = correct_rejections
+        stats_dict_all[talker][pitch_param]['correct_response'] = correct_rejections
         stats_dict_all[talker][pitch_param]['dprime'] = CalculateStats.dprime(hits, false_alarms)
 
     return stats_dict_all, stats_dict
@@ -185,11 +193,11 @@ def plot_stats(stats_dict_all_combined, stats_dict_combined):
 
             color = color_map(
                 np.where(np.array(list(measurement.keys())) == talker)[0][0])  # Assign color based on label
-            rects = ax3.bar(offset, measurement_data['correct_rejections'], width, label=talker, color=color)
+            rects = ax3.bar(offset, measurement_data['correct_response'], width, label=talker, color=color)
             #scatter plot the corresponding individual ferret data, each ferret is a different marker shape
             marker_list = ['o', 's', '<', 'd', "*"]
             count = 0
-            for ferret, ferret_data in stats_dict_combined[attribute][talker]['correct_rejections'].items():
+            for ferret, ferret_data in stats_dict_combined[attribute][talker]['correct_response'].items():
                 #add jitter to offset
                 print('ferret', ferret)
                 print('ferret data', ferret_data)
@@ -203,7 +211,7 @@ def plot_stats(stats_dict_all_combined, stats_dict_combined):
     ax3.set_xticks([0.25, 1.25], ['Female', 'Male'])
 
     ax3.set_ylabel('P(CR)')
-    ax3.set_title('Correct rejections')
+    ax3.set_title('Correct responses')
     multiplier = 0
     for attribute, measurement in stats_dict_all_combined.items():
         for talker, measurement_data in measurement.items():
@@ -251,8 +259,8 @@ def plot_stats(stats_dict_all_combined, stats_dict_combined):
     ax3.annotate('c)', xy=get_axis_limits(ax3), xytext=(-0.1, ax3.title.get_position()[1]+0.1), textcoords='axes fraction', fontproperties = font_props,zorder=10)
     ax4.annotate('d)', xy=get_axis_limits(ax4), xytext=(-0.1, ax4.title.get_position()[1]+0.1), textcoords='axes fraction', fontproperties = font_props,zorder=10)
 
-    plt.suptitle('Proportion of hits, false alarms and d\' by talker')
-    plt.savefig('figs/proportion_hits_falsealarms_dprime_bytalker.png', dpi = 500, bbox_inches='tight')
+    plt.suptitle('Proportion of hits, false alarms, correct responses and d\' by talker')
+    plt.savefig('figs/proportion_hits_falsealarms_correctresp_dprime_bytalker.png', dpi = 500, bbox_inches='tight')
     plt.show()
 
 
